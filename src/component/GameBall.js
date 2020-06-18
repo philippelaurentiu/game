d4f13283460553ebe9c0f4e47e600a6de4a03443
phil <laurentiu@publicplan.de>
2020-06-18 10:59:11 +0200

    save current changes

--- src/component/GameBall.js
+++ src/component/GameBall.js
@@ -4,20 +4,49 @@ class GameBall extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      delete: false
+    };
+
+    this.removeBallByClick = this.removeBallByClick.bind(this);
   }
 
   render() {
+
     const position = {
       top: this.props.posY + 'px',
-      left: this.props.posX +'px'
+      left: this.props.posX +'px',
     };
 
-    return (
-      <div style={position} className={'GameBall'}>
+    if (!this.state.delete) {
+      this.removeBallByTime();
+      return (
+        <div style={position} className={'GameBall'} onClick={this.removeBallByClick}>
+        </div>
+      );
+    }
 
-      </div>
-    );
+    if (this.state.delete) {
+      return null;
+    }
   }
-};
+
+  removeBallByTime () {
+    setTimeout(() => {
+      this.setState(
+        {delete: true
+        });
+    }, 5000);
+  }
+
+  removeBallByClick() {
+    this.setState(
+      {
+        delete: true
+      });
+  }
+
+}
 
 export default GameBall
--- src/component/GameBord.js
+++ src/component/GameBord.js
@@ -6,12 +6,18 @@ class GameBord extends Component {
   constructor(props) {
     super(props);
   }
-
   render() {
+
+    const min = 1;
+    const max = 400;
+    const rand = min + Math.random() * (max - min);
+
+    console.log(rand);
+    console.log(parseInt(rand , 10 ));
 
     return (
       <div className={'GameBord'}>
-        <GameBall posX={100} posY={200} />
+        <GameBall posX={rand} posY={rand} />
       </div>
     );
   }
